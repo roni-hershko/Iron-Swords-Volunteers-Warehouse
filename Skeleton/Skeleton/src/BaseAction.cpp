7cedee63c54dad4ae4497898e4be505f8722ba2f
Gal Patel <galpat@post.bgu.ac.il>
2024-01-18 11:37:34 +0000

    13:37

--- Skeleton/Skeleton/src/BaseAction.cpp
+++ Skeleton/Skeleton/src/BaseAction.cpp
@@ -18,3 +18,45 @@ void BaseAction::error(string errorMsg){
 string BaseAction::getErrorMsg() const{
     return errorMsg;
 } //return the error message
+
+
+//order
+Order::Order(int id):customerId(id){} //constructor
+
+void Order::act(WareHouse &wareHouse){
+    wareHouse.addOrder(this);
+} //add order to warehouse, maybe error
+
+string Order::toString() const{
+    if(getStatus()==ActionStatus::COMPLETED)
+        return "Order Completed";
+    return "Order Error: "+getErrorMsg();
+}
+
+Order *Order::clone() const{
+    
+     return new Order(customerId);
+}
+   
+   
+AddCustomer::AddCustomer(string customerName, string customerType, int distance, int maxOrders)
+    :customerName(customerName), distance(distance),  maxOrders(maxOrders){
+        if(customerType.at(0)=='c' || customerType.at(0)=='C') 
+             this->customerType = CustomerType::Civilian;
+        else this->customerType = CustomerType::Soldier;
+    }//constructor
+
+
+void customerName::act(WareHouse &wareHouse){
+    wareHouse.addCustomer(this);
+} //add customer to warehouse, never error
+
+customerName *AddCustomer::clone() const{
+    return new AddCustomer(customerName, customerType,  distance,  maxOrders);
+}
+string customerName::toString() const{
+    if(getStatus()==ActionStatus::COMPLETED)
+        return "AddCustomer Completed";
+    return "AddCustomer Error: "+getErrorMsg();
+}
+
